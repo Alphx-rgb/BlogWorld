3f97957f1d8df1117b68c0bff972b7a441565383
Ayush Budhiraja <78094309+Alphx-rgb@users.noreply.github.com>
2021-12-01 02:21:19 +0530

    Login Page for the Website

---
+++ login.js
@@ -0,0 +1,17 @@
+frm_2=document.getElementById("form_2")
+forms_1=document.getElementById("form_1")
+bkgrnd=document.getElementById("background")
+frm_2.style.display="none";
+console.log("AAAAA");
+btn=document.getElementById("sign_up");
+
+btn.onclick = function form_2(){
+    forms_1.style.display="none";
+    frm_2.style.display="block";
+    frm_2.style.width="500px";
+    frm_2.style.height="600px";
+    bkgrnd.style.width="500px";
+    bkgrnd.style.height="600px";
+    console.log("BBBBB");
+}
+
